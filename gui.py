7c644da4600d22aa4a0efe85c0ceccc1d63accef
Shreyas Ashtamkar <51398602+Shreyas-Ashtamkar@users.noreply.github.com>
2024-05-11 11:28:57 +0530

    Disclaimer: WorkInProgress

--- gui.py
+++ gui.py
@@ -1,5 +1,23 @@
-"""
-To be implemented :
+from writing_wizard import WritingWizard
 
-Streamlit GUI calling Writing Wizard and displaying messages
-"""
+import streamlit as st
+from streamlit_js_eval import streamlit_js_eval
+
+st.set_page_config(layout='wide')
+
+st.warning("Disclaimer: This GUI is WorkInProgress.")
+st.title("Writing Wizard")
+
+with st.container(border=True):
+    st.header("Article Writing")
+    if input := st.chat_input("Provide a topic :"):
+        WritingWizard.chat(input)
+        
+    with st.container(border=False):
+        for message in WritingWizard.messages:
+            with st.chat_message(message['role']):
+                st.markdown(message['content'])
+    
+
+with st.sidebar:
+    st.title("Cool")
